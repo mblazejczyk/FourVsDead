8174ff45853a4d0437e8dc1709e1fdd2382e4603
Mateusz Błażejczyk <nibek1000@gmail.com>
2025-11-23 16:41:43 +0100

    Add singleplayer web app demo
    Adding web app demo version of game

---
+++ docs/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Mateusz Blazejczyk-Four VS dead-1.0.1 demo";
+const contentToCache = [
+    "Build/4vd-demo-final.loader.js",
+    "Build/4vd-demo-final.framework.js",
+    "Build/4vd-demo-final.data",
+    "Build/4vd-demo-final.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
